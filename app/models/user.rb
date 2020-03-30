032e29c29739f1f0ce728aea66f9723dcff79d5e
Charlotte Mannion <charlottemannion100@gmail.com>
2020-03-30 12:23:41 +0100

    a user can sign up, log in and log out of the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true 
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
+# Rails.application.routes.draw do
+#   get 'sessions/new'
+#   get 'sessions/create'
+#   get 'sessions/destroy'
+#   resources :users
+#   root to: "spaces#index"
+# end
+
 Rails.application.routes.draw do
+  #   get 'sessions/new'
+  #   get 'sessions/create'
+  #   get 'sessions/destroy'
+  #   resources :users
+  #   root to: "spaces#index"
+
+  root 'spaces#index'
+
   resources :users
-  root to: "spaces#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
