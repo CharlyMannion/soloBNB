bcca793eb6f2fb26f000e46caf6fb58e5cd8eb7e
Charlotte Mannion <charlottemannion100@gmail.com>
2020-04-01 12:26:57 +0100

    only the owner of a listing can edit or destroy the listing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :current_username
 
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
+    end
+  end
+
+  def current_username
+    if session[:user_id]
+      current_user ||= User.find(session[:user_id])
+      @current_username = current_user.username
+    else
+      @current_username = nil
     end
   end
 end
